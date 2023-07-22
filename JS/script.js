5497dc11e892979c0a3c8d0fe59b132b614a8824
BuildTools <unconfigured@null.spigotmc.org>
2023-07-22 06:38:34 +0300

    mod: Изменил JS массив Указал необходимые фото для игры в JS массиве.
    add: Необходимые фото
    Добавил Необходимые фото для игры

--- JS/script.js
+++ JS/script.js
@@ -1,15 +1,93 @@
 // Массив с данными о фотографиях
 const charactersData = [
-    {
-      name: "Имя_Персонажа_1",
-      surname: "Фамилия_Персонажа_1",
-      photoUrl: "URL_Фотографии_1"
-    },
-    {
-      name: "Имя_Персонажа_2",
-      surname: "Фамилия_Персонажа_2",
-      photoUrl: "URL_Фотографии_2"
-    },
-    // Я ненадолго прервусь и добавлю сюда необходимых персонажей... Выше вы видите пример, как я это буду делать.
-  ];
-  
+  {
+    name: "Грегори",
+    surname: "Хаус",
+    photoUrl: "IMAGES/18.jpg"
+  },
+  {
+    name: "Йен",
+    surname: "Арчер",
+    photoUrl: "IMAGES/17.jpg"
+  },
+  {
+    name: "Джек",
+    surname: "Бауэр",
+    photoUrl: "IMAGES/16.jpg"
+  },
+  {
+    name: "Джон",
+    surname: "Макклейн",
+    photoUrl: "IMAGES/15.jpg"
+  },
+  {
+    name: "Шерлок",
+    surname: "Холмс",
+    photoUrl: "IMAGES/14.jpg"
+  },
+  {
+    name: "Мардж",
+    surname: "Симпсон",
+    photoUrl: "IMAGES/13.jpg"
+  },
+  {
+    name: "Иван",
+    surname: "Будько",
+    photoUrl: "IMAGES/12.jpg"
+  },
+  {
+    name: "Майкл",
+    surname: "Майерс",
+    photoUrl: "IMAGES/11.jpg"
+  },
+  {
+    name: "Майкл",
+    surname: "Скофилд",
+    photoUrl: "IMAGES/10.jpg"
+  },
+  {
+    name: "Дин",
+    surname: "Винчестер",
+    photoUrl: "IMAGES/9.jpg"
+  },
+  {
+    name: "Перси",
+    surname: "Джексон",
+    photoUrl: "IMAGES/8.jpg"
+  },
+  {
+    name: "Брюс",
+    surname: "Уэйн",
+    photoUrl: "IMAGES/7.jpg"
+  },
+  {
+    name: "Джесс",
+    surname: "Ааронс",
+    photoUrl: "IMAGES/6.jpg"
+  },
+  {
+    name: "Доминик",
+    surname: "Торетто",
+    photoUrl: "IMAGES/5.jpg"
+  },
+  {
+    name: "Питер",
+    surname: "Паркер",
+    photoUrl: "IMAGES/4.jpg"
+  },
+  {
+    name: "Гектор",
+    surname: "Барбосса",
+    photoUrl: "IMAGES/3.jpg"
+  },
+  {
+    name: "Гарри",
+    surname: "Поттер",
+    photoUrl: "IMAGES/1.jpg"
+  },
+  {
+    name: "Стив",
+    surname: "Роджерс",
+    photoUrl: "IMAGES/2.jpg"
+  },
+];
